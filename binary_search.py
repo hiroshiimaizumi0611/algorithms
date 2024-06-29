40ea1e7ada4b9901025f9c76d61b5f45fdb4e5b5
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-06-29 13:22:36 +0900

    binary_search

--- binary_search.py
+++ binary_search.py
@@ -1,40 +1,40 @@
-def binary_search(a: list, key: int) -> bool:
-    n = len(a)
+def linear_serch(numbers: list[int], value: int) -> int:
+    for i in range(0, len(numbers)):
+        if numbers[i] == value:
+            return i
+    return -1
 
-    l = -1
-    r = n
 
-    while r - l > 1:
-        m = (l + r) // 2
+# def binary_search(numbers: list, value: int) -> int:
+#     left, right = 0, len(numbers) - 1
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if numbers[mid] == value:
+#             return mid
+#         elif numbers[mid] < value:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+#     return -1
 
-        if a[m] == key:
-            return True
 
-        if a[m] > key:
-            r = m
+def binary_search(numbers: list, value: int) -> int:
+    def recursive(numbers: list[int], value: int, left: int, right: int) -> int:
+        if left > right:
+            return -1
+
+        mid = (left + right) // 2
+
+        if numbers[mid] == value:
+            return mid
+        elif numbers[mid] < value:
+            return recursive(numbers, value, mid + 1, right)
         else:
-            l = m
+            return recursive(numbers, value, left, mid - 1)
 
-    return False
-
-
-def main():
-    a = [
-        3,
-        1,
-        4,
-        1,
-        5,
-        9,
-        2,
-        6,
-        5,
-    ]
-    a.sort()
-
-    key = 1
-    print(binary_search(a, key))
+    return recursive(numbers, value, 0, len(numbers) - 1)
 
 
 if __name__ == "__main__":
-    main()
+    nums = [0, 1, 5, 7, 9, 11, 15, 20, 24]
+    print("index num = " + str(binary_search(nums, 15)))
