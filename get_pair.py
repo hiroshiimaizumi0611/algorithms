bef3621020c413716af3ce626bcf01f0f1f6eb8a
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-07-11 11:48:41 +0900

    get_pair

---
+++ get_pair.py
@@ -0,0 +1,31 @@
+from typing import Optional, Tuple
+
+
+def get_pair(numbers: list[int], target: int) -> Optional[Tuple[int, int]]:
+    cache = set()
+    for num in numbers:
+        val = target - num
+        if val in cache:
+            return val, num
+        cache.add(num)
+
+
+def get_pair_half_sum(numbers: list[int]) -> Optional[Tuple[int, int]]:
+    sum_numbers = sum(numbers)
+    if sum_numbers % 2 != 0:
+        return
+    half_sum = int(sum_numbers / 2)
+
+    cache = set()
+    for num in numbers:
+        cache.add(num)
+        val = half_sum - num
+        if val in cache:
+            return val, num
+
+
+if __name__ == "__main__":
+    l = [11, 2, 5, 9, 10, 3]
+    t = 12
+    print(get_pair(l, t))
+    print(get_pair_half_sum(l))
