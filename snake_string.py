33aa5aef70774eba1e0e1947c1905b549ec2008a
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-07-30 15:14:21 +0900

    v2

--- snake_string.py
+++ snake_string.py
@@ -1,3 +1,7 @@
+import operator
+import string
+
+
 def snake_string_v1(chars: str) -> list[list[str]]:
     result = [[], [], []]
     result_indexes = {0, 1, 2}
@@ -15,8 +19,36 @@ def snake_string_v1(chars: str) -> list[list[str]]:
     return result
 
 
+def snake_string_v2(chars: str, depth: int) -> list[list[str]]:
+    result = [[] for _ in range(depth)]
+    result_indexes = {i for i in range(depth)}
+    insert_index = int(depth / 2)
+
+    # pos = lambda i: i + 1
+    # neg = lambda i: i - 1
+
+    op = operator.neg
+
+    for s in chars:
+        result[insert_index].append(s)
+        for rest_index in result_indexes - {insert_index}:
+            result[rest_index].append(" ")
+        if insert_index <= 0:
+            op = operator.pos
+        if insert_index >= depth - 1:
+            op = operator.neg
+        insert_index += op(1)
+
+    return result
+
+
 if __name__ == "__main__":
-    numbers = [str(i) for j in range(5) for i in range(10)]
-    strings = "".join(numbers)
-    for line in snake_string_v1(strings):
+    # numbers = [str(i) for j in range(5) for i in range(10)]
+    # strings = "".join(numbers)
+    # for line in snake_string_v1(strings):
+    #     print("".join(line))
+
+    alphabet = [s for _ in range(2) for s in string.ascii_lowercase]
+    strings = "".join(alphabet)
+    for line in snake_string_v2(strings, 6):
         print("".join(line))
