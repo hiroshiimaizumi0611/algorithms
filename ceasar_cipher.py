454e94ac9d8af567e47f3c3b8b86ff9313101fae
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-08-16 11:45:55 +0900

    carsar_cipher_hack

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -1,4 +1,5 @@
 import string
+from typing import Generator, Tuple
 
 
 def caesar_cipher(text: str, shift: int) -> str:
@@ -18,8 +19,29 @@ def caesar_cipher(text: str, shift: int) -> str:
     return result
 
 
+def carsar_cipher_hack(text: str) -> Generator[Tuple[int, str], None, None]:
+    len_alphabet = ord("Z") - ord("A") + 1
+    len_alphabet = len(string.ascii_uppercase)
+    for candidate_shift in range(1, len_alphabet + 1):
+        reverted = ""
+        for char in text:
+            if char.isupper():
+                alphabet = string.ascii_uppercase
+            elif char.islower():
+                alphabet = string.ascii_lowercase
+            else:
+                reverted += char
+                continue
+
+            index = alphabet.index(char) - candidate_shift
+            if index < 0:
+                index += len_alphabet
+            reverted += alphabet[index]
+        yield candidate_shift, reverted
+
+
 if __name__ == "__main__":
     e = caesar_cipher("ATTACK SILICON VALLY by eginner", 3)
     print(e)
-    d = caesar_cipher(e, -3)
-    print(d)
+    for shift_num, decrypted in carsar_cipher_hack(e):
+        print(f"{shift_num:2d}", decrypted)
