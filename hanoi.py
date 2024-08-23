720d3656262207c1cb31ec3e77114993dc5399c4
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-08-23 11:06:11 +0900

    hanoi_movement

--- hanoi.py
+++ hanoi.py
@@ -7,5 +7,23 @@ def hanoi(disk: int, src: str, dest: str, support: str):
     hanoi(disk - 1, support, dest, src)
 
 
+def get_hanoi_movement(
+    disk: int, src: str, dest: str, support: str
+) -> list[tuple[int, str, str]]:
+    result = []
+
+    def _hanoi(disk: int, src: str, dest: str, support: str):
+        if disk < 1:
+            return
+
+        _hanoi(disk - 1, src, support, dest)
+        result.append((disk, src, dest))
+        _hanoi(disk - 1, support, dest, src)
+
+    _hanoi(disk, src, dest, support)
+
+
 if __name__ == "__main__":
     hanoi(3, "A", "C", "B")
+    for r in get_hanoi_movement(4, "A", "C", "B"):
+        print(r)
